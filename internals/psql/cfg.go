ad88db93185e1fa274428710aa2740d64c53469f
daniel <aciddaniel@gmail.com>
2021-07-23 12:06:48 +0300

    reusable postgress client package

---
+++ internals/psql/cfg.go
@@ -0,0 +1,45 @@
+package psql
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+type Config struct {
+	dbUser  string
+	dbPswd  string
+	dbHost  string
+	dbPort  string
+	dbName  string
+	migrate string
+}
+
+func GetPGCfg() *Config {
+	conf := &Config{}
+
+	flag.StringVar(&conf.dbUser, "dbuser", os.Getenv("POSTGRES_USER"), "DB user name")
+	flag.StringVar(&conf.dbPswd, "dbpswd", os.Getenv("POSTGRES_PASSWORD"), "DB pass")
+	flag.StringVar(&conf.dbPort, "dbport", os.Getenv("POSTGRES_PORT"), "DB port")
+	flag.StringVar(&conf.dbHost, "dbhost", os.Getenv("POSTGRES_HOST"), "DB host")
+	flag.StringVar(&conf.dbName, "dbname", os.Getenv("POSTGRES_DB"), "DB name")
+
+	flag.Parse()
+
+	return conf
+}
+
+func (c *Config) GetDBConnStr() string {
+	return c.getDBConnStr(c.dbHost, c.dbName)
+}
+
+func (c *Config) getDBConnStr(dbhost, dbname string) string {
+	return fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s?sslmode=disable",
+		c.dbUser,
+		c.dbPswd,
+		dbhost,
+		c.dbPort,
+		dbname,
+	)
+}
