5e5c1de10f6d8af5deaaa2faab3e0ca6391cb9f0
daniel <aciddaniel@gmail.com>
2021-07-23 11:56:16 +0300

    pipeline to keep collection of common interface adapters

---
+++ cmd/crawler/pipeline/pipeline.go
@@ -0,0 +1,37 @@
+package pipeline
+
+import (
+	"github.com/cs-bird/cmd/crawler/adapters"
+	"github.com/cs-bird/cmd/crawler/types"
+)
+
+type Report struct {
+	Err error
+	Res types.Checkpoint
+}
+
+type Pipeline struct {
+	Adapters []adapters.Adapter
+}
+
+func New() *Pipeline {
+	return &Pipeline{}
+}
+
+func (p *Pipeline) Add(a adapters.Adapter) {
+	p.Adapters = append(p.Adapters, a)
+}
+
+func (p *Pipeline) Process() map[string]Report {
+	resp := map[string]Report{}
+
+	for _, a := range p.Adapters {
+		report, err := a.Get()
+		resp[report.Coin] = Report{
+			Err: err,
+			Res: report,
+		}
+	}
+
+	return resp
+}
