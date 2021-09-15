941d3f12cbb23dc89abd10d0b137eff535a86dd8
daniel <aciddaniel@gmail.com>
2021-09-15 16:19:34 +0300

    adding persistence layer to checkpoint

--- cmd/crawler/types/checkpoint.go
+++ cmd/crawler/types/checkpoint.go
@@ -1,6 +1,9 @@
 package types
 
 import (
+	"context"
+
+	"github.com/cs-bird/internals/psql"
 	"github.com/shopspring/decimal"
 )
 
@@ -9,4 +12,30 @@ type Checkpoint struct {
 	Coin    string
 	Address string
 	Balance decimal.Decimal
+	Nonce   int64
+}
+
+func PersistCheckpoint(ctx context.Context, db psql.DBIterator, pld Checkpoint) (err error) {
+	stmt := `
+		INSERT INTO
+		public."checkpoints" (
+			time,
+			coin,
+			address,
+			balance,
+			nonce
+		)
+		VALUES (NOW(), $1, $2, $3, $4)
+	`
+
+	_, err = db.ExecContext(
+		ctx,
+		stmt,
+		pld.Coin,
+		pld.Address,
+		pld.Balance.String(),
+		pld.Nonce,
+	)
+
+	return
 }
