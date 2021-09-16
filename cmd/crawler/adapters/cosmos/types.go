c3a93f9b2513a4b3401e1a6fb8fd84816bba085a
daniel <aciddaniel@gmail.com>
2021-09-16 14:02:57 +0300

    adding additional type for atomscan

--- cmd/crawler/adapters/cosmos/types.go
+++ cmd/crawler/adapters/cosmos/types.go
@@ -2,8 +2,16 @@ package cosmos
 
 import "github.com/shopspring/decimal"
 
-type BalanceReport struct {
+type CosmosstationReport struct {
 	Balances []struct {
 		Amount decimal.Decimal `json:"amount"`
 	}
+}
+
+type AtomscanReport struct {
+	Height string `json:"height"`
+	Result []struct {
+		Denom  string          `json:"denom"`
+		Amount decimal.Decimal `json:"amount"`
+	} `json:"result"`
 }
