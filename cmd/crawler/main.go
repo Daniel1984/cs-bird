eb93c303fa1e2d7b0d370bdbf5a14b9da23f5551
daniel <aciddaniel@gmail.com>
2021-09-16 14:11:36 +0300

    persisting report data to ts db

--- cmd/crawler/main.go
+++ cmd/crawler/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"time"
 
 	"github.com/cs-bird/cmd/crawler/adapters/cosmos"
@@ -9,9 +11,16 @@ import (
 	"github.com/cs-bird/cmd/crawler/adapters/elrond"
 	"github.com/cs-bird/cmd/crawler/adapters/filecoin"
 	"github.com/cs-bird/cmd/crawler/pipeline"
+	"github.com/cs-bird/cmd/crawler/types"
+	"github.com/cs-bird/internals/psql"
 )
 
 func main() {
+	db, err := psql.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	pl := pipeline.New()
 	pl.Add(filecoin.New())
 	pl.Add(elrond.New())
@@ -19,8 +28,18 @@ func main() {
 	pl.Add(dfinity.New())
 
 	for {
-		report := pl.Process()
-		fmt.Printf("pipeline response: %+v\n", report)
-		time.Sleep(60 * time.Second)
+		checkpoints := pl.Process()
+		for coin, cp := range checkpoints {
+			if cp.Err != nil {
+				fmt.Printf("report to slack:> %s -> %+v\n", coin, cp)
+				continue
+			}
+
+			if err := types.PersistCheckpoint(context.TODO(), db.Client, cp.Res); err != nil {
+				fmt.Printf("failed persisting %s:>%+v, msg:%s\n", coin, cp, err)
+			}
+		}
+
+		time.Sleep(120 * time.Second)
 	}
 }
