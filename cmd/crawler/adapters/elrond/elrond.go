357510bc36ca1423b4967eba96fbf289461f75dc
daniel1984 <aciddniel@gmail.com>
2022-05-04 15:45:30 +0200

    updating elrond crawler

--- cmd/crawler/adapters/elrond/elrond.go
+++ cmd/crawler/adapters/elrond/elrond.go
@@ -10,9 +10,10 @@ import (
 )
 
 const (
-	address       = "erd1a56dkgcpwwx6grmcvw9w5vpf9zeq53w3w7n6dmxcpxjry3l7uh2s3h9dtr"
-	elrondURL     = "https://api.elrond.com/accounts/%s"
-	elrondscanURL = "https://api.elrondscan.com/accounts/%s"
+	address   = "erd1a56dkgcpwwx6grmcvw9w5vpf9zeq53w3w7n6dmxcpxjry3l7uh2s3h9dtr"
+	explorer1 = "https://api.elrond.com/accounts/%s"
+	explorer2 = "https://elrondscan.com:3000/accounts/%s"
+	explorer3 = "https://internal-api.elrond.com/accounts/%s"
 )
 
 type Elrond struct {
@@ -29,8 +30,9 @@ func New() *Elrond {
 
 func (e *Elrond) Get() (cp models.Checkpoint, err error) {
 	reporterPool := []reporterpool.Reporter{
-		elrondReport,
-		elrondscanReport,
+		explorer1Report,
+		explorer2Report,
+		explorer3Report,
 	}
 
 	for {
@@ -56,10 +58,10 @@ func (e *Elrond) Get() (cp models.Checkpoint, err error) {
 	return
 }
 
-func elrondReport(address string) (decimal.Decimal, error) {
-	rep := ElrondReport{}
+func explorer1Report(address string) (decimal.Decimal, error) {
+	rep := Explorer1Report{}
 	req := request.
-		New("GET", fmt.Sprintf(elrondURL, address), nil).
+		New("GET", fmt.Sprintf(explorer1, address), nil).
 		Do().
 		Decode(&rep)
 
@@ -70,10 +72,51 @@ func elrondReport(address string) (decimal.Decimal, error) {
 	return rep.Balance, nil
 }
 
-func elrondscanReport(address string) (decimal.Decimal, error) {
-	rep := ElrondScanReport{}
+func explorer2Report(address string) (decimal.Decimal, error) {
+	rep := Explorer2Report{}
 	req := request.
-		New("GET", fmt.Sprintf(elrondscanURL, address), nil).
+		New("GET", fmt.Sprintf(explorer2, address), nil).
+		AddHeaders("Accept", "application/json").
+		AddHeaders("Accept-Language", "en-GB,en;q=0.9,lt-GB;q=0.8,lt;q=0.7,en-US;q=0.6").
+		AddHeaders("Authorization", "WlhKa01YRnhjWEZ4Y1hGeGNYRnhjWEZ4Y1hCeGNYRnhjWEZ4Y1hGeGNYRnhjWEZ4Y1hGeGNYRnhjWEZ4Y1hGeGNYRnhlV3hzYkhOc2JYRTJlVFk2TWpBeU1pMHdOUzB3TkZReE13PT0=").
+		AddHeaders("Connection", "keep-alive").
+		AddHeaders("Origin", "https://elrondscan.com").
+		AddHeaders("Referer", "https://elrondscan.com/").
+		AddHeaders("Sec-Fetch-Dest", "empty").
+		AddHeaders("Sec-Fetch-Mode", "cors").
+		AddHeaders("Sec-Fetch-Site", "same-site").
+		AddHeaders("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36").
+		AddHeaders("X-Requested-With", "XMLHttpRequest").
+		AddHeaders("X-Socket-Id", "319760622.309542742").
+		AddHeaders("sec-ch-ua", `" Not A;Brand";v="99", "Chromium";v="100", "Google Chrome";v="100""`).
+		AddHeaders("sec-ch-ua-mobile", "?0").
+		AddHeaders("sec-ch-ua-platform", "macOS").
+		Do().
+		Decode(&rep)
+
+	if err := req.HasError(); err != nil {
+		return decimal.NewFromInt(0), err
+	}
+
+	return rep.Balance, nil
+}
+
+func explorer3Report(address string) (decimal.Decimal, error) {
+	rep := Explorer3Report{}
+	req := request.
+		New("GET", fmt.Sprintf(explorer3, address), nil).
+		AddHeaders("authority", "internal-api.elrond.com").
+		AddHeaders("accept", "*/*").
+		AddHeaders("accept-language", "en-GB,en;q=0.9,lt-GB;q=0.8,lt;q=0.7,en-US;q=0.6").
+		AddHeaders("authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3NBZGRyZXNzIjoiZXJkMWs0ZHVjMm5leWRod2FucW5ua2ZlN3p2M2QzdG1qMDAzd3Zlc3dsYXBmc3JsbXc0Zjc3eHNqeGFxMmMiLCJzaWduYXR1cmUiOiI2NDVhNTg0Y2QzMjczZDg5NmIyYzQ5MzUyN2U3ZWYwZTJlNDA1NjM2NjhjNWZlZmU3MjZkYzFkMWIwZjFlMGNlNDBkZWQ2M2FiMzc3YzUzY2JhMDJhNTFjNzYxM2RiZDliOGNlZGYxNzNlYWMwNjdmNGFlYTZkYWYwMWI3MDcwMCIsImlhdCI6MTY1MTY3MDY0NCwiZXhwIjoxNjUxNjc3ODQ0fQ.TRfhtEf8Rxe67hgmvsWiyj-mkGA3HG4vv4BWmoBEdfU").
+		AddHeaders("origin", "https://explorer.elrond.com").
+		AddHeaders("sec-ch-ua", `" Not A;Brand";v="99", "Chromium";v="100", "Google Chrome";v="100""`).
+		AddHeaders("sec-ch-ua-mobile", "?0").
+		AddHeaders("sec-ch-ua-platform", "macOS").
+		AddHeaders("sec-fetch-dest", "empty").
+		AddHeaders("sec-fetch-mode", "cors").
+		AddHeaders("sec-fetch-site", "same-site").
+		AddHeaders("user-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36").
 		Do().
 		Decode(&rep)
 
--- cmd/crawler/adapters/elrond/types.go
+++ cmd/crawler/adapters/elrond/types.go
@@ -2,13 +2,19 @@ package elrond
 
 import "github.com/shopspring/decimal"
 
-type ElrondReport struct {
+type Explorer1Report struct {
 	TxCount int             `json:"txCount"`
 	Nonce   int64           `json:"nonce"`
 	Balance decimal.Decimal `json:"balance"`
 }
 
-type ElrondScanReport struct {
+type Explorer2Report struct {
+	TxCount int             `json:"txCount"`
+	Nonce   int64           `json:"nonce"`
+	Balance decimal.Decimal `json:"balance"`
+}
+
+type Explorer3Report struct {
 	TxCount int             `json:"txCount"`
 	Nonce   int64           `json:"nonce"`
 	Balance decimal.Decimal `json:"balance"`
